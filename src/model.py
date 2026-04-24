b043f82515eb5e86dcc800c40557da6fc38f20e3
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-04-24 10:48:21 +0530

    add wallet model with constructor and attributes

---
+++ src/model.py
@@ -0,0 +1,6 @@
+class Wallet:
+    def __init__(self, wallet_id, owner, balance):
+        self.wallet_id = wallet_id
+        self.owner = owner
+        self.balance = balance
+        self.history =  []
