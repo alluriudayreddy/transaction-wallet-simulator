e18840506b6c78f827e483dd529335217b74a80d
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-04-27 09:42:13 +0530

    add main.py tests with try-except error handling

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 from model import Wallet
+from operations import deposit, withdraw
+
 
 w1 = Wallet("w1", "Danam", 1280)
-print("Owner:", w1.owner)
-print("ID:", w1.wallet_id)
-print("Balance:", w1.balance)
+
+try:
+    deposit(w1, 300)
+    withdraw(w1, 200)
+    withdraw(w1, 5000)
+
+except ValueError as e:
+    print("Error:", e)
+
+print(f'balance {w1.balance}')
 print("History:", w1.history)
