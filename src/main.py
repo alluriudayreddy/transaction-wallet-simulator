9d451622f7b6f65592428325a19eda651cb3b35d
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-04-28 12:08:20 +0530

    Test transfer logic and wallet state outputs in main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from model import Wallet
-from operations import deposit, withdraw
+from operations import deposit, withdraw, transfer
 from storage import save_wallet, load_wallet
 
 w1=Wallet("W001", "Danam", 1000)
+w2=Wallet("W002", "Alice", 1500)
 
 deposit(w1, 500)
+transfer(w1, w2, 300)
 withdraw(w1, 200)
 
 save_wallet(w1)
@@ -14,3 +16,8 @@ loaded_wallet = load_wallet()
 print(f'Owner: {loaded_wallet.owner}')
 print(f'balance: {loaded_wallet.balance}')
 print(f'History: {loaded_wallet.history}')
+
+print(f'Sender Balance: {w1.balance}')
+print(f'Receiver Balance: {w2.balance}')
+print(f'Sender History: {w1.history}')
+print(f'Receiver History: {w2.history}')
