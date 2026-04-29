b262d5fc5d37557bcc2389375441ff196cb516ae
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-04-29 15:04:31 +0530

    Imrpove naming clarity in helper functions

--- src/helpers.py
+++ src/helpers.py
@@ -4,19 +4,19 @@ def generate_wallet_id():
     return f'W{random.randint(1000,9999)}'
 
 
-def show_wallet(wallet):
-    print(f"Wallet ID: {wallet.wallet_id}")
-    print(f'Owner: {wallet.owner}')
-    print(f'Balance: {wallet.balance}')
+def show_wallet(wallet_account):
+    print(f"Wallet ID: {wallet_account.wallet_id}")
+    print(f'Owner: {wallet_account.owner}')
+    print(f'Balance: {wallet_account.balance}')
 
 
-def show_history(wallet):
+def show_history(wallet_account):
     print("History:")
-    for item in wallet.history:
+    for item in wallet_account.history:
         print(item)
 
 
-def show_all_wallets(wallets):
+def show_all_wallets(all_wallets):
     print("All Wallets: ")
-    for wallet_id, wallet in wallets.items():
-        print(f'{wallet_id}: {wallet.owner} - Balance: {wallet.balance} - History: {wallet.history}')
+    for wallet_id, wallet_account in all_wallets.items():
+        print(f'{wallet_id}: {wallet_account.owner} - Balance: {wallet_account.balance} - History: {wallet_account.history}')
