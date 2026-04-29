ec9124de683e0c8c2c974ebdcaf7f0918461f007
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-04-29 14:59:29 +0530

    Rename Wallet class to WalletAccount for clearer domain modeling

--- src/model.py
+++ src/model.py
@@ -1,7 +1,7 @@
-class Wallet:
+class WalletAccount:
+    
     def __init__(self, wallet_id, owner, balance):
         self.wallet_id = wallet_id
         self.owner = owner
         self.balance = balance
-        self.history =  []
-        
+        self.history = []
